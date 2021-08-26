88ce3b36f8e8fcba2282049204b49bc729ca13f6
Juan-Code-Gomez <jgomezberrio054@gmail.com>
2021-08-26 09:59:17 -0500

    Se hace el front de la mini escuela

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './home/login/login.component';
+import { RegisterCourseComponent } from './user/register-course/register-course.component';
+import { RegisterGroupComponent } from './user/register-group/register-group.component';
+import { RegisterStudentComponent } from './user/register-student/register-student.component';
+import { RegisterTeacherComponent } from './user/register-teacher/register-teacher.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'registerStudent',
+    component: RegisterStudentComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'registerTeacher',
+    component: RegisterTeacherComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'registerGroup',
+    component: RegisterGroupComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'registerCourse',
+    component: RegisterCourseComponent,
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,51 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './home/header/header.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { LoginComponent } from './home/login/login.component';
+import { RegisterStudentComponent } from './user/register-student/register-student.component';
+import { RegisterTeacherComponent } from './user/register-teacher/register-teacher.component';
+import { RegisterCourseComponent } from './user/register-course/register-course.component';
+import { RegisterGroupComponent } from './user/register-group/register-group.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterStudentComponent,
+    RegisterTeacherComponent,
+    RegisterCourseComponent,
+    RegisterGroupComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
+    MatSnackBarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
